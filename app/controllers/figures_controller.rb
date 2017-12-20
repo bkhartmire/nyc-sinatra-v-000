3ea41de689268525d8a0b7e71e306bdba972ba9e
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-20 18:29:25 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,10 @@
 class FiguresController < ApplicationController
 
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(name: params[:figure[name]])
+  end
 end
